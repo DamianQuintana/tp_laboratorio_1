6f324151c47d79c8f15163f9ae32bab5d3fc3876
DamianQuintana <52511412+DamianQuintana@users.noreply.github.com>
2019-09-04 18:18:29 -0300

    Add files via upload
    Ademas de subir los archivos por separado subi uno comprimido

---
+++ Funciones.h
@@ -0,0 +1,57 @@
+
+/** \brief Pide al usuario que ingrese un numero por teclado, tanto valor entero como valores con coma
+ *
+ * \return double Nos devuelve el valor del numero ingresado por el usuario
+ *
+ */
+double getNumber(); // Prototipo de la funcion que solicita los valores
+
+
+/** \brief Toma dos valores y los resta
+ *
+ * \param number1 double Primer valor a ser restado
+ * \param number2 double Segundo valor
+ * \return double Devuelve el resultado de la resta
+ *
+ */
+double subtraction(double number1, double number2); // Prototipo de la funcion resta
+
+
+/** \brief Toma dos valores y los suma
+ *
+ * \param number1 double Primer valor a sumar
+ * \param number2 double Segundo valor a sumar
+ * \return double Devuelve el resultado de la suma
+ *
+ */
+double sum(double number1, double number2); // Prototipo de la funcion suma
+
+
+/** \brief Toma dos valores y los multiplica
+ *
+ * \param number1 double Primer valor para la operacion
+ * \param number2 double Segundo valor para la operacion
+ * \return double Devuelve el resultado de la multiplicacion
+ *
+ */
+double multiplication(double number1, double number2); // Prototipo de la funcion multiplicacion
+
+
+/** \brief Toma dos valores y los divide
+ *
+ * \param number1 double Primer valor, dividendo
+ * \param number2 double Segundo valor, divisor
+ * \return double Devuelve el resultado de la division
+ *
+ */
+double division(double number1, double number2); // Prototipo de la funcion division
+
+
+/** \brief Toma dos valores y averigua el factorial de cada uno
+ *
+ * \param number1 double Primer valor
+ * \param number2 double Segundo valor
+ * \return double Devuelve el valor del factorial de ambos numeros
+ *
+ */
+double factorial(double number1, double number2); // Prototipo de la funcion de factorizacion
